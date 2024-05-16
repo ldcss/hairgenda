7e8b4ebd091ad8a9b7562201dd8ec1578bf8a7a3
ldcss <lucass.dcss75@gmail.com>
2024-05-16 09:15:16 -0300

    <feat> create project and first screen

---
+++ hairgenda/hairgendaApp.swift
@@ -0,0 +1,17 @@
+//
+//  hairgendaApp.swift
+//  hairgenda
+//
+//  Created by Lucas Daniel Costa da Silva on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct hairgendaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
